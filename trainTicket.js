28db4ba98b39dfe5158c68c9f058ac4886298924
malexC137 <71665644+malexC137@users.noreply.github.com>
2021-04-08 17:07:23 +0200

    Draft

--- trainTicket.js
+++ trainTicket.js
@@ -1,5 +1,6 @@
 var kms = prompt("Quanti chilometri vuoi percorrere?");
-var age = prompt("Qual è la tua età?");
+// var age = prompt("Qual è la tua età?");
+var age = Math.floor(Math.random() * (100 - 1) + 1);
 var costPerKm = 0.21
 var minAge = 20
 var minDiscount = 0.2
@@ -11,11 +12,15 @@ var totCost = (parseFloat((kms * costPerKm).toFixed(2)));
 var totCostMin = (parseFloat((kms * minCostPerKm).toFixed(2)));
 var totCostMax = (parseFloat((kms * maxCostPerKm).toFixed(2)));
 
+console.log(age);
 
 if (age < minAge) {
     console.log(totCostMin);
+    document.getElementById("ticketCost").innerHTML = totCostMin + " Euro";
 } else if (age > maxAge) {
     console.log(totCostMax);
+    document.getElementById("ticketCost").innerHTML = totCostMax + " Euro";
 } else {
     console.log(totCost);
+    document.getElementById("ticketCost").innerHTML = totCost + " Euro";
 };
